6db3a3f568d06ce388c816f09a5b2d4ef64c41fa
Gaurav Kanger <747695@student.tor.loc>
2019-08-07 16:14:03 -0400

    Second commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const data={
+firstName:'Laurence',
+lastName:'Svekis'
+}
+const site =http.createServer(function(req,res){
+console.log('Hello World');
+console.log(req.headers);
+console.log(req.url);
+res.setHeader('Content-Type','application/json');
+res.write(JSON.stringify(data));
+res.end();
+});
+site.listen(3000);
